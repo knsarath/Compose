ea7c03b6b800190a93b44af7f7dc102163f86b7e
sarath-goibibo <sarath.n@go-mmt.com>
2021-08-29 23:21:31 +0530

    Simple card view using Box and column

--- app/src/main/java/com/app/compose/MainActivity.kt
+++ app/src/main/java/com/app/compose/MainActivity.kt
@@ -3,36 +3,103 @@ package com.app.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.app.compose.ui.theme.ComposeTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.W400
+import androidx.compose.ui.text.font.FontWeight.Companion.W600
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            Column(modifier = Modifier.padding(16.dp)) {
+                Box(modifier = Modifier.fillMaxWidth(0.5f)) {
+                    ImageCard(
+                        painter = painterResource(id = R.drawable.bike),
+                        contentDescription = "Canyon mountain Bike",
+                        title = "Canyon",
+                        modifier = Modifier.fillMaxWidth()
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Android")
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier
+) {
+
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp
+    ) {
+
+        Box(modifier = Modifier.size(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Fit
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomStart)
+                    .background(
+                        Brush.verticalGradient(
+                            colors = (listOf(Color.Transparent, Color.Black))
+                        )
+                    ),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Column(modifier = Modifier.align(Alignment.Center)) {
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        text = title,
+                        color = Color.White,
+                        fontSize = 18.sp, fontStyle = FontStyle.Italic,
+                        fontWeight = W600,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = contentDescription,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(bottom = 8.dp),
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        fontStyle = FontStyle.Normal,
+                        fontWeight = W400
+                    )
+                }
+            }
+        }
+
     }
+
+
 }
